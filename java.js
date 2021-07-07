17b23dcd71f17bae3ade83bcbb8c71d58b918013
Cintia Cocuzza <cintiagcocuzza@gmail.com>
2021-07-07 12:12:17 -0300

    commit pagina casi lista

---
+++ java.js
@@ -0,0 +1,10 @@
+/* listado de prestaciones*/
+
+var listado de prestacines=document.querySelector(listado de prestaciones)
+listado de prestaciones.overmouse=function functionName() {
+  listado de prestaciones.style.color="black"
+}
+var video=document.querySelector(video)
+video.overmouse=function functionName(){
+  video.
+}
