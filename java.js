148b123b721d160dce01597326eae40c97965773
Cintia Cocuzza <cintiagcocuzza@gmail.com>
2021-07-07 22:09:05 -0300

    pagina definitiva

--- java.js
+++ java.js
@@ -1,10 +1,4 @@
-/* listado de prestaciones*/
+/* galeria*/
 
-var listado de prestacines=document.querySelector(listado de prestaciones)
-listado de prestaciones.overmouse=function functionName() {
-  listado de prestaciones.style.color="black"
-}
-var video=document.querySelector(video)
-video.overmouse=function functionName(){
-  video.
-}
+
+var
